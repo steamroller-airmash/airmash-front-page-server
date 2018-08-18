d5cb2ac7d17315bffbbc7bb43d77f7e9e3e2b5bf
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2018-08-18 17:50:34 -0400

    Refactor endpoints into separate functions

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,10 @@ mod spec;
 
 use actix_web::error::InternalError;
 use actix_web::http::StatusCode;
-use actix_web::{http, middleware, server, App, Error, HttpResponse};
+use actix_web::{http, middleware, server, App, Error, HttpResponse, HttpRequest};
 use futures::future::join_all;
 use futures::{Future, Stream};
+use http::Method;
 
 use sentry::{Hub, Level};
 use sentry_actix::{SentryMiddleware, ActixWebHubExt};
@@ -38,6 +39,96 @@ lazy_static! {
 	static ref CONFIG: GameSpec = serde_json::from_str(CONFIG_FILE).unwrap();
 }
 
+/// Make an http request to all gameservers
+/// to query the number of players that they
+/// have
+fn games(req: &HttpRequest) -> Box<Future<Item = HttpResponse, Error = Error>> {
+	let https = HttpsConnector::new(4).unwrap();
+	let client: Client<_, Body> = Client::builder()
+		.build(https);
+
+	let headers = req.request().headers();
+	let country = headers.get("CF-IPCountry")
+		.map(|x| x.to_str().unwrap_or("XX"))
+		.unwrap_or("XX")
+		.to_owned();
+
+	let mut regions = vec![];
+	let config = CONFIG.clone();
+
+	for region in config.data.iter() {
+		let mut requests = vec![];
+
+		for server in region.games.iter() {
+			requests.push(
+				client
+					.get(server.url.parse().unwrap())
+					.and_then(|response| {
+						response.into_body().fold(
+							vec![],
+							|mut acc, chunk| -> Result<Vec<_>, hyper::Error> {
+								acc.extend_from_slice(&*chunk);
+								Ok(acc)
+							},
+						)
+					})
+					.map(|v: Vec<u8>| {
+						serde_json::from_slice(&v).unwrap()
+					})
+					.map(|v: ServerResponse| v.players)
+					.map_err(|e| {
+						error!("Error: {:?}", e);
+						InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR)
+					}),
+			);
+		}
+
+		let region = region.clone();
+
+		regions.push(join_all(requests).map(move |counts| {
+			let games = region
+				.games
+				.into_iter()
+				.zip(counts.into_iter())
+				.map(|(game, count)| ServerSpec {
+					players: count,
+					..game
+				})
+				.collect();
+
+			RegionSpec { games, ..region }
+		}));
+	}
+
+	Box::new(
+		join_all(regions)
+			.map(|regions| GameSpec {
+				protocol: 5,
+				country: country,
+				data: regions,
+			})
+			.map(|spec| serde_json::to_string(&spec).unwrap())
+			.map(|resp| {
+				HttpResponse::Ok()
+					.header(http::header::CONTENT_TYPE, "application/json; charset=utf-8")
+					.body(resp)
+			})
+			.map_err(|e| e.into()),
+	)
+}
+
+fn clienterror(req: &HttpRequest) -> HttpResponse {
+	let hub = Hub::from_request(req);
+	hub.capture_message("A client error occurred", Level::Error);
+
+	HttpResponse::Ok()
+		.body("")
+}
+
+fn login(_: &HttpRequest) -> HttpResponse {
+	HttpResponse::NotImplemented().body("")
+}
+
 fn main() {
 	std::env::set_var("RUST_LOG", "info");
 	std::env::set_var("RUST_BACKTRACE", "1");
@@ -48,95 +139,9 @@ fn main() {
 		App::new()
 			.middleware(middleware::Logger::default())
 			.middleware(SentryMiddleware::new())
-			.resource("/games", move |r| {
-				r.method(http::Method::GET).f(
-					move |req| -> Box<Future<Item = HttpResponse, Error = Error>> {
-						let https = HttpsConnector::new(4).unwrap();
-						let client: Client<_, Body> = Client::builder()
-							.build(https);
-
-						let headers = req.request().headers();
-						let country = headers.get("CF-IPCountry")
-							.map(|x| x.to_str().unwrap_or("XX"))
-							.unwrap_or("XX")
-							.to_owned();
-
-						let mut regions = vec![];
-						let config = CONFIG.clone();
-
-						for region in config.data.iter() {
-							let mut requests = vec![];
-
-							for server in region.games.iter() {
-								requests.push(
-									client
-										.get(server.url.parse().unwrap())
-										.and_then(|response| {
-											response.into_body().fold(
-												vec![],
-												|mut acc, chunk| -> Result<Vec<_>, hyper::Error> {
-													acc.extend_from_slice(&*chunk);
-													Ok(acc)
-												},
-											)
-										})
-										.map(|v: Vec<u8>| {
-											serde_json::from_slice(&v).unwrap()
-										})
-										.map(|v: ServerResponse| v.players)
-										.map_err(|e| {
-											error!("Error: {:?}", e);
-											InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR)
-										}),
-								);
-							}
-
-							let region = region.clone();
-
-							regions.push(join_all(requests).map(move |counts| {
-								let games = region
-									.games
-									.into_iter()
-									.zip(counts.into_iter())
-									.map(|(game, count)| ServerSpec {
-										players: count,
-										..game
-									})
-									.collect();
-
-								RegionSpec { games, ..region }
-							}));
-						}
-
-						Box::new(
-							join_all(regions)
-								.map(|regions| GameSpec {
-									protocol: 5,
-									country: country,
-									data: regions,
-								})
-								.map(|spec| serde_json::to_string(&spec).unwrap())
-								.map(|resp| {
-									HttpResponse::Ok()
-										.header(http::header::CONTENT_TYPE, "application/json; charset=utf-8")
-										.body(resp)
-								})
-								.map_err(|e| e.into()),
-						)
-					},
-				)
-			})
-			.resource("/clienterror", move |r| {
-				r.method(http::Method::POST).f(
-					move |req| -> HttpResponse {
-						let hub = Hub::from_request(req);
-						hub.capture_message("A client error occurred", Level::Error);
-
-						HttpResponse::Ok()
-							.body("")
-					},
-				)
-			})
+			.resource("/games", |r| r.method(Method::GET).f(games))
+			.resource("/clienterror", |r| r.method(Method::POST).f(clienterror))
+			.resource("/login", |r| r.method(Method::POST).f(login))
 	}).bind("0.0.0.0:9000")
 		.unwrap()
 		.shutdown_timeout(1)
